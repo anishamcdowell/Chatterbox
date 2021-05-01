10a4ae8f5072caaa176a819d0b9c552f28048fdd
Anisha McDowell <anishamcdowell@gmail.com>
2021-05-01 13:48:05 -0400

    Forum routes in progress

--- controller/htmlRoutes/index.js
+++ controller/htmlRoutes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 // const forumRoutes = require('./forumRoutes');
-// const postRoutes = require('./postRoutes');
+const postRoutes = require('./postRoutes');
 // const commentRoutes = require('./commentRoutes');
 
 router.use('/', homeRoutes);
 // router.use('/forums', forumRoutes);
-// router.use('/posts', postRoutes);
+router.use('/posts', postRoutes);
 // router.use('/comments', commentRoutes);
 
 module.exports = router;
